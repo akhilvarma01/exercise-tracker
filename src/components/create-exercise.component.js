5b7ba03fbbbcaf7bcb075e6aa086793ac83cdb25
akhilvarma01 <akhilvarma01@gmail.com>
2019-07-10 00:21:35 -0500

    second commit

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -30,7 +30,6 @@ export default class CreateExercise extends Component {
             if (response.data.length > 0) {
                 this.setState({
                     users: response.data.map(user => user.username),
-                    username : response.data[0].username
                 })
             }
         })
